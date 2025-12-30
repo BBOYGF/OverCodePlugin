22a66bcbcc07f431d45e13a54a4e85fedf940f5b
GitHub Action <action@github.com>
2025-12-30 17:03:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/bboygf/autocoderplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bboygf/autocoderplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.bboygf.autocoderplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bboygf.autocoderplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
